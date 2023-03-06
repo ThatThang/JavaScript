94712a31d84233f059487bd413300fcc886a75c8
ThatThang <tonthatthang168@gmail.com>
2023-03-06 10:09:57 +0700

    Update Lession about string

---
+++ source.js
@@ -0,0 +1,39 @@
+var myString = 'Hoc JS cung JS f8 ';
+
+// 1:Length - Tính độ dài của chuỗi 
+// console.log(myString.length)
+
+// 2:Find Index - Tìm vị trí của phần tử trong chuỗi
+// console.log(myString.IndexOf('JS'));
+// console.log(myString.lastIndexOf('JS'));
+
+// 3:Cut String - Cắt chuỗi
+// console.log(myString.slice(4, 6));
+// console.log(myString.slice(-3, -1))
+
+// 4:Replace - Thay thế chuỗi
+// console.log(myString.replace(/JS/g, 'JavaScript'));
+
+// 5:Convert to UperCase - Chuyển chuỗi thành in Hoa
+// console.log(myString.toUpperCase())
+
+// 6:Convert to LowerCase - Chuyển chuỗi thành in Thường
+// console.log(myString.toLocaleLowerCase())
+
+// 7:Trim - Loại bỏ khoảng trắng ở hai đầu của chuỗi
+// console.log(myString.trim().length)
+
+// 8:Split - Tách chuỗi thành array
+
+// var language = 'Js, Php, Ruby';
+
+// console.log(language.split(', '))
+
+// 9: Get a character by index - Lấy phần tử trong chuỗi
+
+// var string = 'Character'
+// console.log(string.charAt(1))
+// Khi nhập một giá trị khác trong chuỗi giá trị trả về sẽ là  chuỗi
+
+// console.log(string[0])
+// Khi nhập một giá trị khác trong chuỗi giá trị trả về sẽ là undefine
